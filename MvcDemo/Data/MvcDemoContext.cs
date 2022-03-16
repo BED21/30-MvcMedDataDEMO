c8f2912cd70fbd8a445b849a9878ae22b1dd42c3
Claes Engelin <claes.engelin@zocom.se>
2022-03-16 12:26:55 +0100

    Add project files.

---
+++ MvcDemo/Data/MvcDemoContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcDemo.Models;
+
+namespace MvcDemo.Data
+{
+    public class MvcDemoContext : DbContext
+    {
+        public MvcDemoContext (DbContextOptions<MvcDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcDemo.Models.Student> Student { get; set; }
+    }
+}
---
+++ MvcDemo/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace MvcDemo.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    [DisplayName("Date of birth")]
+    public DateTime DateOfBirth { get; set; }
+}
